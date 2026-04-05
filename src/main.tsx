192b661dc4646d994ea634b1507fca8a2af4961e
kili <ysxye29@gmail.com>
2026-04-05 14:07:42 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
+// TRƯỚC:
 import { BrowserRouter } from 'react-router-dom'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </StrictMode>
+)
+
+// SAU:
+import { HashRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 )
